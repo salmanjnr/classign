dfb42e6c07797cca161cb6250be663a6663a80a9
salman69e27 <asbayomy04@gmail.com>
2021-01-17 22:37:13 +0200

    die spammer die

--- classign/backend/test/canvas.py
+++ classign/backend/test/canvas.py
@@ -16,11 +16,31 @@ class TestCanvas(unittest.TestCase):
 
     def test_list_assignments(self):
         courses = self.canvas.list_courses()
-        self.assertEqual(courses[0].id, 1841767)
+        self.assertEqual(courses[0].id, '1841767')
 
     def test_get_assignemnts(self):
         assignments = self.canvas.get_assignments()
-        self.assertEqual(len(assignments), 10)
+        print(assignments[0])
+        self.assertTrue(len(assignments) != 0)
+
+    def test_complex_assignments(self):
+        simple_assignmnents = self.canvas.get_assignments()
+        assignment_1 = simple_assignmnents[0]
+        complex_assignment_1 = assignment_1.make_canvas_assignment_object()
+        print(complex_assignment_1)
+
+    def test_file_upload_submission(self):
+        simple_assignmnents = self.canvas.get_assignments()
+        sample_assignment = None
+        for assignment in simple_assignmnents:
+            if assignment.id == '19728737':
+                sample_assignment = assignment
+        sample_assignment = sample_assignment.make_canvas_assignment_object()
+        sample_assignment.online_text_submission.submit('hi')
+        sample_assignment.online_upload_submission.submit(url_list=['https://images.theconversati'
+                                                                    'on.com/files/93616/original/'
+                                                                    'image-20150902-6700-t2axrz.j'
+                                                                    'pg'], comment='hi')
 
 
 unittest.main()

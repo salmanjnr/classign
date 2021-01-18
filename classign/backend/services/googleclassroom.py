5c5f686dd625176c42b355b8ac51fc9165a06172
Abdelrahman Nawar <abdonawar80@gmail.com>
2021-01-18 17:55:51 +0200

    not done yet

--- classign/backend/services/googleclassroom.py
+++ classign/backend/services/googleclassroom.py
@@ -53,26 +53,17 @@ class GoogleClassroom(LMS):
         courses = results.get('courses' , [])
 
         #initialize an empty list to store coures names in it
-        courses_name = []
+        courses_list = []
         #Looping to store all course names
-        assignments = []
+
         if not courses:
             print('No Courses found')
         else:
             for course in courses:
-                courses_name.append(course['name'])
-        if not courses:
-            print('No Courses found')
-        else:
-            for course in courses:
-                work_results = x.courses().courseWork().list(
-                courseId=course['id']).execute()
-                work = work_results.get("courseWork" , [] )
+                courses_list.append(self.__make_course_object(course))
 
-                for assignment in  work:
-                    assignments.append(assignment['title'])
+        return  courses_list
 
-        return  assignments
 
     def bulk_download(self):
         # TODO
@@ -80,6 +71,43 @@ class GoogleClassroom(LMS):
 
     def get_assignments(self):
         pass
+    def __make_course_object(self, course_dict):
+        course_id = str(course_dict['id'])
+        name = course_dict['name']
+        return CanvasCourse(course_id, name)
+class GoogleClasroomCourse(Course):
+    def __init__(self, course_id, name):
+        self.id = course_id
+        self.name = name
+    def get_assignments(self):
+        x = self.authenticate()
+        # Requesting courses from GoogleClasroom API after authenticating
+        work_results = x.courses().courseWork().list(
+        courseId=course[self.id]).execute()
+        work = work_results.get("courseWork" , [] )
+        assignments = []
+        for assignment in work:
+            assignments.append(self.__make_simple_assignment_object(assignment))
 
-x = GoogleClassroom()
-print(x.list_courses())
+        return assignments
+
+    def bulk_download(self):
+        # TODO
+        pass
+    def __make_simple_assignment_object(self, assignment_dict):
+        assignment_id = str(assignment_dict['id'])
+        name = assignment_dict['title']
+        due = assignment_dict['dueDate']
+
+        due_date = str(due['month']) + '/' + str(due['day'])
+        return SimpleGoogleClassroomAssignment(assignment_id, self.id, name, due_date)
+
+    def __str__(self):
+        return "course_id={}\ncourse_name={}\n".format(self.id,self.name )
+class SimpleGoogleClassroomAssignment:
+    def __init__(self, assignment_id, course_id, name, due_date):
+        self.id = assignment_id
+        self.course_id = course_id
+        self.name = name
+        self.due_date = due_date
+        
